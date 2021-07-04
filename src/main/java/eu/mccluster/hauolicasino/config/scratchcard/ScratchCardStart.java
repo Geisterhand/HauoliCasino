bffd1b63c130a533911e989d1add0d296a8a0c24
BuildTools <unconfigured@null.spigotmc.org>
2021-07-04 17:58:36 +0200

    Removed double chance

--- src/main/java/eu/mccluster/hauolicasino/config/scratchcard/ScratchCardStart.java
+++ src/main/java/eu/mccluster/hauolicasino/config/scratchcard/ScratchCardStart.java
@@ -15,10 +15,6 @@ public class ScratchCardStart extends Config {
     public ScratchCardStart(File file) { _lootFile = file; }
 
     @Order(1)
-    @Comment("Chance that a button is a reward. Chance is rolled for every single one of the 9 buttons.")
-    public int chance = 10;
-
-    @Order(2)
     public ScratchCardConfig loottable = new ScratchCardConfig();
 
     @Override
